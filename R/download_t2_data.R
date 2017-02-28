7edeb40e972aee73847eff2698e93f43e296dc89
muschellij2 <muschellij2@gmail.com>
2017-02-27 23:28:20 -0500

    adding readme

---
+++ R/download_t2_data.R
@@ -0,0 +1,16 @@
+#' @title Download T2 Image Filenames
+#'
+#' @description Download the files for the T2 images
+#' @param ... arguments to pass to \code{\link{download_kirby21_data}},
+#' \code{modalities = "T2"} so it cannot be specified
+#' @return Indicator if the data is downloaded.
+#' 
+#' @examples
+#' download_t2_data()
+#' @export
+#' @importFrom kirby21.base download_kirby21_data
+download_t2_data =  function(...) {  
+  x = kirby21.base::download_kirby21_data(modality = "T2", ...)
+  return(x)
+}
+
