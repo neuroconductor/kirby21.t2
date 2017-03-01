dc78c650a5ac7c2925e082f05fdee0c159f89f63
muschellij2 <muschellij2@gmail.com>
2017-03-01 15:20:33 -0500

    adding readme

--- R/download_t2_data.R
+++ R/download_t2_data.R
@@ -5,10 +5,8 @@
 #' \code{modalities = "T2"} so it cannot be specified
 #' @return Indicator if the data is downloaded.
 #' 
-#' @examples
-#' download_t2_data()
 #' @export
-#' @importFrom kirby21.base download_kirby21_data
+#' @importFrom kirby21.base download_kirby21_data all_modalities
 download_t2_data =  function(...) {  
   x = kirby21.base::download_kirby21_data(modality = "T2", ...)
   return(x)
