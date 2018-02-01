40130d12419c0496eba83a8f91b07ae1e86f347e
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:07:28 -0500

    updated description with doi

--- R/get_t2_filenames.R
+++ R/get_t2_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the T2 images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "T2"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_t2_filenames()
